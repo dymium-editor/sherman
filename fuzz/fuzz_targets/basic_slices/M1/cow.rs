96458602bb325d39ea9983dce94d595cefa1687e
sharnoff <29154784+sharnoff@users.noreply.github.com>
2022-12-11 18:45:41 -0800

    fuzz: add targets for M=1

---
+++ fuzz/fuzz_targets/basic_slices/M1/cow.rs
@@ -0,0 +1,43 @@
+#![no_main]
+
+use arbitrary::{Arbitrary, Unstructured};
+use libfuzzer_sys::fuzz_target;
+use sherman::{param::AllowCow, Constant};
+use sherman_fuzz_utils::{CommandSequence, CowCommand, ExtraRunnerInfo, Name, RunnerState};
+use std::fmt::{self, Debug, Formatter};
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+struct UpperLetter(char);
+
+// Forward the Debug implementation to the inner `char` so that printing the fuzz input can be more
+// easily replicated.
+impl Debug for UpperLetter {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<'d> Arbitrary<'d> for UpperLetter {
+    fn arbitrary(u: &mut Unstructured<'d>) -> arbitrary::Result<Self> {
+        Ok(UpperLetter((b'A' + u.int_in_range(0_u8..=25)?) as char))
+    }
+}
+
+struct TyName;
+
+impl Name for TyName {
+    fn name() -> &'static str {
+        "CowFuzzTree<1>"
+    }
+}
+
+fuzz_target!(|cmds: CommandSequence<TyName, CowCommand<u8, Constant<UpperLetter>>>| {
+    let cmds = cmds.map(|cmd| cmd.map_slice(|c| Constant(c.0)));
+
+    let mut runner: RunnerState<u8, Constant<UpperLetter>, AllowCow, 1> = RunnerState::init();
+    let extra = ExtraRunnerInfo::new_cow();
+
+    for c in cmds.cmds {
+        runner.run_cow_cmd(&c, &extra);
+    }
+});
---
+++ fuzz/fuzz_targets/basic_slices/M1/slice_ref.rs
@@ -0,0 +1,43 @@
+#![no_main]
+
+use arbitrary::{Arbitrary, Unstructured};
+use libfuzzer_sys::fuzz_target;
+use sherman::{param::AllowSliceRefs, Constant};
+use sherman_fuzz_utils::{CommandSequence, ExtraRunnerInfo, Name, RunnerState, SliceRefCommand};
+use std::fmt::{self, Debug, Formatter};
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+struct UpperLetter(char);
+
+// Forward the Debug implementation to the inner `char` so that printing the fuzz input can be more
+// easily replicated.
+impl Debug for UpperLetter {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<'d> Arbitrary<'d> for UpperLetter {
+    fn arbitrary(u: &mut Unstructured<'d>) -> arbitrary::Result<Self> {
+        Ok(UpperLetter((b'A' + u.int_in_range(0_u8..=25)?) as char))
+    }
+}
+
+struct TyName;
+
+impl Name for TyName {
+    fn name() -> &'static str {
+        "SliceRefFuzzTree<1>"
+    }
+}
+
+fuzz_target!(|cmds: CommandSequence<TyName, SliceRefCommand<u8, Constant<UpperLetter>>>| {
+    let cmds = cmds.map(|cmd| cmd.map_slice(|c| Constant(c.0)));
+
+    let mut runner: RunnerState<u8, Constant<UpperLetter>, AllowSliceRefs, 1> = RunnerState::init();
+    let extra = ExtraRunnerInfo::new_slice_refs();
+
+    for c in cmds.cmds {
+        runner.run_slice_ref_cmd(&c, &extra);
+    }
+});
